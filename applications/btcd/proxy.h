290d1f60b3e75600fd17827cb4e4094eac7de80c
Michael Gronager <gronager@MacGronager.local>
2011-12-08 13:33:30 +0100

    renamed btcproxy to btcd

---
+++ applications/btcd/proxy.h
@@ -0,0 +1,34 @@
+// Copyright (c) 2011 Michael Gronager
+// Distributed under the MIT/X11 software license, see the accompanying
+// file license.txt or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef _BITCOIN_PROXY_H_
+#define _BITCOIN_PROXY_H_
+
+#include "btcHTTP/Server.h"
+
+json_spirit::Value gettxmaturity(const json_spirit::Array& params, bool fHelp);
+//json_spirit::Value gettxdetails(const json_spirit::Array& params, bool fHelp);
+json_spirit::Value getvalue(const json_spirit::Array& params, bool fHelp);
+//json_spirit::Value getdebit(const json_spirit::Array& params, bool fHelp);
+//json_spirit::Value getcredit(const json_spirit::Array& params, bool fHelp);
+json_spirit::Value getcoins(const json_spirit::Array& params, bool fHelp);
+json_spirit::Value posttx(const json_spirit::Array& params, bool fHelp);
+json_spirit::Value checkvalue(const json_spirit::Array& params, bool fHelp);
+
+class GetDebit : public Method {
+public:
+    virtual json_spirit::Value operator()(const json_spirit::Array& params, bool fHelp);
+};
+
+class GetCredit : public Method {
+public:
+    virtual json_spirit::Value operator()(const json_spirit::Array& params, bool fHelp);
+};
+
+class GetTxDetails : public Method {
+public:
+    virtual json_spirit::Value operator()(const json_spirit::Array& params, bool fHelp);
+};
+
+#endif
