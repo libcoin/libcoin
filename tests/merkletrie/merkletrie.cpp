f1b059bf607edd7500a80a6dc4661c46d70bdb2f
Jeremy Salwen <jeremysalwen@gmail.com>
2013-12-25 02:27:06 -0500

    Removed x86 assembly, replaced with fast portable algorithm.

--- tests/merkletrie/merkletrie.cpp
+++ tests/merkletrie/merkletrie.cpp
@@ -27,13 +27,28 @@
 
 #include <stdint.h>
 
+//Portable algorithms from http://aggregate.org/MAGIC/
+unsigned uint32_t ones32(uint32_t x)
+{
+        /* 32-bit recursive reduction using SWAR...
+	   but first step is mapping 2-bit values
+	   into sum of 2 1-bit values in sneaky way
+	*/
+        x -= ((x >> 1) & 0x55555555);
+        x = (((x >> 2) & 0x33333333) + (x & 0x33333333));
+        x = (((x >> 4) + x) & 0x0f0f0f0f);
+        x += (x >> 8);
+        x += (x >> 16);
+        return(x & 0x0000003f);
+}
+
 static inline uint32_t log2(const uint32_t x) {
-    uint32_t y;
-    asm ( "\tbsr %1, %0\n"
-         : "=r"(y)
-         : "r" (x)
-         );
-    return y;
+    x |= (x >> 1);
+    x |= (x >> 2);
+    x |= (x >> 4);
+    x |= (x >> 8);
+    x |= (x >> 16);
+    return(ones32(x >> 1));
 }
 
 using namespace std;
