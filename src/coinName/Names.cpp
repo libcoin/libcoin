319ea9c85ea79294752301b622152fe0bf1215ae
Daniel Kraft <d@domob.eu>
2014-03-16 21:19:33 +0100

    Add source file for Names.h.

---
+++ src/coinName/Names.cpp
@@ -0,0 +1,18 @@
+/* -*-c++-*- libcoin - Copyright (C) 2014 Daniel Kraft
+ *
+ * libcoin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * libcoin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libcoin.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <coinName/Names.h>
+
